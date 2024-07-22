95dfb0e4d867af9c0ca4c1724df714ef3ea5acdd
pedro420b <badrxxxpedro@gmail.com>
2024-07-22 13:56:56 +0100

    v00001

---
+++ lib/bloc/auth/auth_bloc.dart
@@ -0,0 +1,55 @@
+
+
+import 'auth_event.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'auth_state.dart';
+
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  final AuthRepository authRepository;
+
+  AuthBloc({required this.authRepository}) : super(UnAuthenticated()) {
+   
+on<SignUpRequested>((event, emit) async {
+      emit(Loading());
+      try {
+        await authRepository.signUp(
+            email: event.email, password: event.password);
+        emit(Authenticated());
+      } catch (e) {
+        emit(AuthError(e.toString()));
+        emit(UnAuthenticated());
+      }
+    });
+    
+on<SignInRequested>((event, emit) async {
+      emit(Loading());
+      try {
+        await authRepository.signIn(
+            email: event.email, password: event.password);
+        emit(Authenticated());
+      } catch (e) {
+        emit(AuthError(e.toString()));
+        emit(UnAuthenticated());
+      }
+    });
+    
+    on<GoogleSignInRequested>((event, emit) async {
+      emit(Loading());
+      try {
+        await authRepository.signInWithGoogle();
+        emit(Authenticated());
+      } catch (e) {
+        emit(AuthError(e.toString()));
+        emit(UnAuthenticated());
+      }
+    });
+
+    on<SignOutRequested>((event, emit) async {
+      emit(Loading());
+      await authRepository.signOut();
+      emit(UnAuthenticated());
+    });
+  }
+}
